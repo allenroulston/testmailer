f37b6fb855010f5007d49df9e3e9fb9a5183dd64
prdpspkt <prdpspkt@gmail.com>
2016-08-04 21:16:56 +0545

    changed some

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
- 
-    def welcome_email(user)
-      @user = user
-      @url  = 'http://example.com/login'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-    end
-  
+	default from: 'myemail@example.com'
+
+	def welcome_email(user)
+		@user = user
+		@url  = 'http://www.example.com'
+		mail(to: @user.email, subject: 'Welcome to example.com')
+	end
 end
